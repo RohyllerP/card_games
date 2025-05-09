5bd57c120c4eaa47559d1adac952a67df40a81af
RohyllerP <rohyler@gmail.com>
2025-05-09 23:49:48 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'card_games',
+  webDir: 'www'
+};
+
+export default config;
